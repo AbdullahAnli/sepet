243f671f80f3a480fbdd0fead57e997ac7980c78
Abdullah Anli <anliabdullah08@gmail.com>
2024-03-20 16:51:52 +0300

    Service classları oluşturuldu

---
+++ src/main/java/com/sepet/SepetApplication.java
@@ -0,0 +1,13 @@
+package com.sepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepetApplication.class, args);
+	}
+
+}
